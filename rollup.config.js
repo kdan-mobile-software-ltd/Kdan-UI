b1e0dac486cfb431681f691df558387c130b0299
Jay <xingyu.chen@kdanmobile.com>
2020-09-03 11:52:41 +0800

    add rollup-plugin-terser

--- rollup.config.js
+++ rollup.config.js
@@ -4,7 +4,7 @@ import external from 'rollup-plugin-peer-deps-external';
 import postcss from 'rollup-plugin-postcss';
 import resolve from '@rollup/plugin-node-resolve';
 import image from '@rollup/plugin-image';
-import visualizer from 'rollup-plugin-visualizer';
+import { terser } from 'rollup-plugin-terser';
 import pkg from './package.json';
 
 export default {
@@ -36,6 +36,6 @@ export default {
       include: ['node_modules/**']
     }),
     image(),
-    visualizer()
+    terser()
   ]
 };
